11a2794c7e6a1f10b7f265c1b9fd2aa54401a7d9
whdcumt <wanghongdong0@126.com>
2016-08-24 16:34:38 +0800

    添加图片名称的字符串函数

--- code.c
+++ code.c
@@ -1,6 +1,12 @@
 #include<stdio.h>  
 int main(void)  
 {  
+  const  char *InputName, *OutputName;
+  InputName = "input.bmp"; OutputName = "output.bmp";
+  //FILE *InputFile, *OutputFile;
+ // bmp_t *bmp = NULL, *blur = NULL;
   printf("Hello, world!\n");  
+  printf("%s\n",InputName);
+  printf("%s\n",OutputName);
   return 0;  
 }  
